53ee497ce880d73fadb599f1576a7a7f4fbaac0a
0xwb7 <woobin200111@gmail.com>
2025-10-18 16:25:18 +0900

    refactor: RegexParser - 구분자가 숫자인 경우 예외 처리 하도록 함.

--- src/main/java/calculator/service/parsing/RegexParser.java
+++ src/main/java/calculator/service/parsing/RegexParser.java
@@ -4,8 +4,6 @@ import java.util.regex.Pattern;
 
 public class RegexParser {
 
-    private static final String DEFAULT_DELIMITER = "[,:]";
-
     public record regex(String delimiterRegex, String payload) {}
 
     public regex parse(String input) {
@@ -20,19 +18,23 @@ public class RegexParser {
             String custom = input.substring(2, nl);
 
             if (custom.isEmpty()) {
-                throw new IllegalArgumentException("custom empty");
+                throw new IllegalArgumentException("empty custom delimiter");
             }
 
             if (custom.length() != 1) {
                 throw new IllegalArgumentException("only one delimiter allowed");
             }
 
+            if (Character.isDigit(custom.charAt(0))) {
+                throw new IllegalArgumentException("number not allowed");
+            }
+
             String payload = input.substring(nl + 2);
-            String delimiter = DEFAULT_DELIMITER + "|" + Pattern.quote(custom); // 커스텀 구분자와 기본 구분자 혼합 사용 가능
+            String delimiter = "[,:]" + "|" + Pattern.quote(custom); // 커스텀 구분자와 기본 구분자 혼합 사용 가능
             return new regex(delimiter, payload);
         }
 
         // 커스텀 구분자 아닌 경우
-        return new regex(DEFAULT_DELIMITER, input);
+        return new regex("[,:]", input);
     }
 }
